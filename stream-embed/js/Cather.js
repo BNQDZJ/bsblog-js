cd01bad24c28e57ddb16152863dad2453cb60f40
Bnq Dzj <64721413+BNQDZJ@users.noreply.github.com>
2023-07-10 22:25:26 +0800

    2023-7-10

--- stream-embed/js/Cather.js
+++
@@ -1,3 +0,0 @@
-async function Apple_mp4_url() {
-    Apple_mp4.src = url;
-}
--- stream-embed/js/Hunter.js
+++
@@ -1,7 +0,0 @@
-async function Apple_mp4_id() {
-    Apple_mp4 = document.getElementById("01VFOUL3H67UOFTFVMFJB2I5XFOJISZUM3")
-    item_id = Apple_mp4.id
-    Preview()
-}
-
-Apple_mp4_id()
