721459b8d451200ad59fad0db496d42aa890412d
Bnq Dzj <64721413+BnqDzj@users.noreply.github.com>
2023-07-10 19:03:14 +0800

    Add files via upload

---
+++ stream-embed/js/Cather.js
@@ -0,0 +1,3 @@
+async function Apple_mp4_F1() {
+    Apple_mp4.src = url;
+}
---
+++ stream-embed/js/Hunter.js
@@ -0,0 +1,7 @@
+async function Apple_mp4_F0() {
+    Apple_mp4 = document.getElementById("01VFOUL3H67UOFTFVMFJB2I5XFOJISZUM3")
+    item_id = Apple_mp4.id
+    Preview()
+}
+
+Apple_mp4_F0()
